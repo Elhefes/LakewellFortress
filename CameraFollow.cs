3e2fb75d144afc0e91db5fe03db2c33b0379f324
joonatri <33051433+joonatri@users.noreply.github.com>
2017-11-21 12:14:42 +0200

    Changes to be committed: 	new file:   CameraFollow.cs

---
+++ CameraFollow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+	
+	private Transform target;
+	void Start () {
+		target = GameObject.Find ("placeholderPlayer").GetComponent<Transform>();
+	}
+	void Update () {
+		if (target) {
+			transform.position = Vector2.Lerp (transform.position, target.position, 0.1f);
+		}
+	}
+}
